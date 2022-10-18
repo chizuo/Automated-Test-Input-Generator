f4c8389bfb3a0c9a280452b059afee1cd022b095
Jonathan <jon.chua51@gmail.com>
2022-10-17 22:48:11 -0700

    updated tests

--- src/test/java/trees/TreeOperationsTest.java
+++ src/test/java/trees/TreeOperationsTest.java
@@ -4,73 +4,37 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.lang.Integer;
 import org.junit.Test;
 
 public class TreeOperationsTest {
-    Node<String> K = new Node<String>("K", null, null);
-    Node<String> J = new Node<String>("J", null, K);
-    Node<String> H = new Node<String>("H", null, null);
-    Node<String> I = new Node<String>("I", H, J);
-    Node<String> C = new Node<String>("C", null, null);
-    Node<String> E = new Node<String>("E", null, null);
-    Node<String> A = new Node<String>("A", null, null);
-    Node<String> D = new Node<String>("D", C, E);
-    Node<String> B = new Node<String>("B", A, D);
-    Node<String> G = new Node<String>("G", null, I);
-    Node<String> F = new Node<String>("F", B, G);
+    int maxNodes;
+    int maxDepth;
+    ArrayList<Node<String>> treeList = new ArrayList<Node<String>>();;
+    ArrayList<String> BFSexpects = new ArrayList<String>();
+
+    public void automatedTestCaseGenerator() {
+        maxNodes = (int) (Math.random() * 100) + 10;
+        for (int i = 0; i < maxNodes; i++) {
+            treeList.add(new Node<String>(Integer.toString(i + 1), null, null));
+        }
+        Collections.shuffle(treeList);
+
+        /* builds the binary tree & the expected order for BFS */
+        for (int i = 0; i < treeList.size(); i++) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            Node<String> parent = treeList.get(i);
+            parent.left = left < treeList.size() ? treeList.get(left) : null;
+            parent.right = right < treeList.size() ? treeList.get(right) : null;
+            BFSexpects.add(parent.contents);
+        }
+    }
 
     @Test
     public void TestBreadthFirstSearchCorrectness() {
-        ArrayList<String> expects = new ArrayList<String>();
-        expects.add("F");
-        expects.add("B");
-        expects.add("G");
-        expects.add("A");
-        expects.add("D");
-        expects.add("I");
-        expects.add("C");
-        expects.add("E");
-        expects.add("H");
-        expects.add("J");
-        expects.add("K");
-        var treeOperations = new TreeOperations();
-        assertEquals(expects, TreeOperations.bfs(F));
-        assertEquals(TreeOperations.bfs(F), treeOperations.bfs(F));
-        assertEquals(TreeOperations.bfs(B), treeOperations.bfs(B));
-        assertEquals(TreeOperations.bfs(G), treeOperations.bfs(G));
-    }
-
-    @Test
-    public void TestPreOrderCorrectness() {
-        ArrayList<String> expects = new ArrayList<String>();
-        expects.add("F");
-        expects.add("B");
-        expects.add("A");
-        expects.add("D");
-        expects.add("C");
-        expects.add("E");
-        expects.add("G");
-        expects.add("I");
-        expects.add("H");
-        expects.add("J");
-        expects.add("K");
-        assertEquals(expects, TreeOperations.preorder(F));
-        var treeOperations = new TreeOperations();
-        assertEquals(TreeOperations.preorder(F), treeOperations.preorder(F));
-        assertEquals(TreeOperations.preorder(B), treeOperations.preorder(B));
-        assertEquals(TreeOperations.preorder(G), treeOperations.preorder(G));
-    }
-
-    @Test
-    public void TestMaxDepthCorrectness() {
-        var treeOperations = new TreeOperations();
-        assertEquals(TreeOperations.maxDepth(F), treeOperations.maxDepth(F));
-        assertEquals(TreeOperations.maxDepth(B), TreeOperations.maxDepth(I));
-        assertTrue(TreeOperations.maxDepth(G) > TreeOperations.maxDepth(B));
-        assertTrue(TreeOperations.maxDepth(G) < TreeOperations.maxDepth(F));
-        assertEquals(5, TreeOperations.maxDepth(F));
-        assertEquals(3, TreeOperations.maxDepth(B));
-        assertEquals(4, TreeOperations.maxDepth(G));
-        assertEquals(2, TreeOperations.maxDepth(D));
+        automatedTestCaseGenerator();
+        assertEquals(TreeOperations.bfs(treeList.get(0)), BFSexpects);
     }
 }
